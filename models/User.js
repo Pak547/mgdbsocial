cdc8bf42737f3094f29613f6a180e88acda71569
Pak <pakapongm@gmail.com>
2024-03-04 14:56:01 -0800

    models folder export from local file

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address']
+//regex to validate email
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+}, {
+  toJSON: {
+    virtuals: true
+  },
+  id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
