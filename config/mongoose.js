d6ca6fdcb7411bb94670e6e49dc09618ae952e50
Pak <pakapongm@gmail.com>
2024-03-04 14:55:04 -0800

    first commit with config file. Please note all code was written locally beforehand

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || 3001, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+
+module.exports = connectDB;
